1a08fde866b48d6e793234a173ab0d87d66593c6
PC1\65676 <alexwolfs997@gmail.com>
2018-10-16 19:24:45 -0700

    E3_2AlejandroLopezGarcia
    metodos

---
+++ E3-2AlejandroLopezGarcia/E3-2AlejandroLopezGarcia/Desarrollo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_2AlejandroLopezGarcia
+{
+    class Desarrollo
+    {
+        // declarando la cola
+        private Queue Kcola; 
+        public void Cola()
+        {
+            // inicializando la cola
+            Kcola = new Queue(); 
+        }
+        // Se añade el objeto  de cualquier tipo a la lista
+        public void EnQueue(object item) 
+        {
+            //  objeto de cualquier tipo de dato.
+            Kcola.Enqueue(item); 
+        }
+
+        public void DeQueue()
+        {
+            // Se elimina el valor en la posicion 0 del arraylist
+            Kcola.Dequeue();
+        }
+    }
+}
---
+++ E3-2AlejandroLopezGarcia/E3-2AlejandroLopezGarcia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_2AlejandroLopezGarcia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se crea la cola 
+            Queue miCola = new Queue();
+            // for para llenar la Cola 
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write("Introdur el valor de cola: ");
+                //  lee el valor introducido para despues guardar en la variable de tipo Var para posterior realizar llamada al elemento
+                var Elem = Console.ReadLine();
+                // llamar a la funcion Enqueue para guardar las variables del elemento que se le asignan.
+                miCola.Enqueue(Elem); 
+            }
+            // Se elimina el primer elemento de la cola
+            miCola.Dequeue();
+            // Foreach para imprimir los elementos de la cola
+            foreach (var Elemento in miCola) 
+            {
+                Console.WriteLine(Elemento);
+            }
+            // Establece la capacidad en el numero real de elementos que hay en la cola
+            miCola.TrimToSize(); 
+            Console.ReadKey();
+        }
+    }
+}
